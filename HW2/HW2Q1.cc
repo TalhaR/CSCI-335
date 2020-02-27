828aa0d7c664d84d5857c953c69c00519b29f3b2
talhar <delta.talha@gmail.com>
2020-02-27 12:49:02 -0500

    fixed error of '=' instead of '=='

--- HW2/HW2Q1.cc
+++ HW2/HW2Q1.cc
@@ -1,20 +1,18 @@
-#include <iostream>
-#include <vector>
+/**
+ * @author Talha Rahman
+ *  Class: CSCI-335
+ *  Program descrption: this is a global function that returns
+ *  an iterator to an object x, if it exists within the bounds 
+ *  of start and end. Else if the element is not found then it 
+ *  will return end.
+ */
 
 template<typename Iterator, typename object>
 Iterator my_find(Iterator start, Iterator end, const object& x) {
-    for(Iterator itr = start; i < end; ++itr) {
-        if (itr = x) {
+    for(Iterator itr = start; itr < end; ++itr) {
+        if (*itr == x) {
             return itr;
         }
     }
     return end;
-}
-
-int main() {
-    std::vector<int> vec{1, 2, 3, 4, 5};
-    
-    std::vector<int>::iterator itr;
-    
-    my_find(vec.begin(), vec.end(), 4);
 }
